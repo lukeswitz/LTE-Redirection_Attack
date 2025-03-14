3f0be26fa94fb65bae7140d6d7973d4140a30c37
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2025-03-14 20:32:22 +0100

    Update redir.sh

--- scripts/redir.sh
+++ scripts/redir.sh
@@ -4,7 +4,7 @@
 #
 
 echo "Have fun..."
-sudo docker run -it -d --device /dev/bus/usb:/dev/bus/usb -v /dev:/dev redirect_4_2g-example LTE_fdd_enodeb
+sudo docker run --net host -it -d -v /dev:/dev --privileged --device /dev/bus/usb:/dev/bus/usb -v /dev:/dev redirect_4_2g-example LTE_fdd_enodeb
 pushd $(dirname $0) > /dev/null
 MYPATH=$PWD
 popd > /dev/null
