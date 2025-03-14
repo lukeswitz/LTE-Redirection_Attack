d47fd8d0f0c165c1eaffbc40b64bf5a564cd3869
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2025-03-14 20:07:39 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -18,9 +18,9 @@ myuser=$(who am i | awk '{print $1}')
 sudo bash srsran_performance
 read -p "Forwarding Interface, device ? " interface
 sudo bash srsepc_if_masq.sh interface
-read -p "Forwarding Interface, ip ? " ip
-sudo sed -i -e "s/gprs nsvc 0 remote ip 127.0.0.1/gprs nsvc 0 remote ip `echo $ip`" osmo_egprs/configs/osmo-bsc.cfg
-sudo sed -i -e "s/listen 127.0.0.1/listen `echo $ip`" osmo_egprs/configs/osmo-sgsn.cfg
+read -p "Forwarding Interface, ip ? " ipin
+sudo sed -i -e "s/0 remote ip 127.0.0.1/0 remote ip `echo $ipin`/g" osmo_egprs/configs/osmo-bsc.cfg
+sudo sed -i -e "s/listen 127.0.0.1/listen `echo $ipin`/g" osmo_egprs/configs/osmo-sgsn.cfg
 read -p "Restart docker: [Y/n]: " restart_docker
 if [ -z $restart_docker ]; then restart_docker="Y";fi
 if [ $restart_docker != "n" ]; then sudo systemctl restart docker;fi
@@ -41,3 +41,4 @@ gnome-terminal -- bash -c "bash 2G.sh; exec bash"
 gnome-terminal -- bash -c "bash redir.sh; exec bash"
 gnome-terminal -- bash -c "bash asterisk.sh; exec bash"
 telnet 172.17.0.2 30001
+
